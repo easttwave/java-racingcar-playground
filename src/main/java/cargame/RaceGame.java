1f0d978e635be684f3901b2a281af8cd3d201486
easttwave <hd.lee6@cj.net>
2024-08-29 16:53:14 +0900

    feat: RaceGame 구현

---
+++ src/main/java/cargame/RaceGame.java
@@ -0,0 +1,18 @@
+package cargame;
+
+/**
+ * @author : lhd
+ * @packageName : cargame
+ * @fileName : java-racingcar-playground
+ * @description :
+ * <p>
+ * ====================================================
+ * DATE            AUTHOR              NOTE
+ * ====================================================
+ * 2024-08-29         lhd
+ */
+public class RaceGame {
+    public static void main(String[] args) {
+
+    }
+}
